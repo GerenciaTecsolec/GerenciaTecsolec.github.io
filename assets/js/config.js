d17dce0cae5dd32cb2fdafc2b6fa1175f5c03a36
aguachis <alvarog_1010@hotmail.com>
2022-10-28 19:45:57 -0500

    primera publicacion

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var dataConfig = {
+    "AMBIENTE": "PRUEBAS", 
+    "SESION_TIME_OUT": 4000,
+    //"URL_LOGIN": "http://localhost:4200",
+    "URL_LOGIN": "http://www.tecsolec.com/nicole",
+    //"URL_DOCUMENTOS": "http://localhost:6000/api/Documentos/GenerarDocumento",
+    "URL_DOCUMENTOS": "http://www.tecsolec.com/nicoledocumentos/api/Documentos/GenerarDocumento",
+    "CARACTERES": "$#@_",
+}
